3a8f8acd60eab9a155cd2b3ad9b98849d4e71d41
IA3005 <81386632+IA3005@users.noreply.github.com>
2024-03-05 05:37:06 +0100

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -27,10 +27,4 @@ def t_wishart_rvs(scale,n,df,size):
     return np.einsum('...,...ij->...ij',vec,L@ws@L.T) 
 
 
-def kotz_wishart_rvs(scale,n,a,b,r,size):
-    p,_ = scale.shape
-    L = la.cholesky(scale)
-    ws = wishart.rvs(scale=np.eye(p),df=n,size=size)
-    qs = math.pow(r,-1/b)*gengamma.rvs(a=(a-1+0.5*n*p)/b,c=b,size=size)
-    vec = qs/np.trace(ws,axis1=-1,axis2=-2)
-    return np.einsum('...,...ij->...ij',vec,L@ws@L.T)
+
